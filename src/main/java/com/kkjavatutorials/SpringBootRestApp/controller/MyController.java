4a6c8785643be114108216f949b42e400f5af536
padmarajuraju <111412403+padmarajuraju@users.noreply.github.com>
2022-08-27 15:38:38 +0530

    Update MyController.java

--- src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
+++ src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
@@ -8,7 +8,7 @@ public class MyController {
 	
 	@GetMapping(path = "/welcome")
 	public String welcome() {
-		return "jenkins assignment--Design and develop tasks required to build CICD pipeline using learned technologies to deploy 2-Tier application on cloud platform as a set of microservices on containerized platform such as docker. Deploy a web application on Docker container using   GRADLE as build tool to install, test, package application which is fetched from GitHub Repository & deploy through Web Application . Provision Infrastructure on AWS.";
+		return "hello guys";
 	}
 
 }
